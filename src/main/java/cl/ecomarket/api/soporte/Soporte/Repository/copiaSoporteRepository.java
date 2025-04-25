6cdd56fca1d6f6c0b7e3652ee1b69284b7c3279c
Renato <ren.ortegar@duocuc.cl>
2025-04-24 20:36:50 -0400

    Tercer Commit

--- src/main/java/cl/ecomarket/api/soporte/Soporte/Repository/copiaSoporteRepository.java
+++ src/main/java/cl/ecomarket/api/soporte/Soporte/Repository/copiaSoporteRepository.java
@@ -6,15 +6,21 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import cl.ecomarket.api.soporte.Soporte.Model.CopiaMonitoreo;
+import cl.ecomarket.api.soporte.Soporte.Model.Monitoreo;
 
 @Repository
 public class copiaSoporteRepository {
 
+    private soporteRepository sp;
     private List<CopiaMonitoreo> listaCopiasMonitoreo = new ArrayList<>();
 
     public List<CopiaMonitoreo> obtenerCopias() {
         return listaCopiasMonitoreo;
-
     }
 
+    public CopiaMonitoreo respaldarCopia(Monitoreo monitoreo){
+        CopiaMonitoreo copy = new CopiaMonitoreo();
+        copy.setVaucher(sp.solicitarSoporte(monitoreo, monitoreo.getId()));
+        return copy;
+    }
 }
